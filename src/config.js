92bb1457b39421006d1cd9745e606fb6d7d52f49
leichain <87617429+leichain@users.noreply.github.com>
2022-03-30 07:25:12 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0xFD86D63748a6390E4a80739e776463088811774D",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1284",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "Moonbeam Punks Dashboard",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 5. (Optional) Set your banner image
-    "banner_picture": "https://www.superflexfitness.com/wp-content/uploads/2017/03/3D-banner-background.jpg",
+    "banner_picture": "https://img.tofunft.com/image/https%3A%2F%2Fimg.tofunft.com%2Fipfs%2FQmeMAPftxNzyMTvvkWSVR5kPz6i8SXq8j1FYgTmzh4R5P8%2F1831.png/1440.png",
   },
 
   // Supported networks - just for your reference
